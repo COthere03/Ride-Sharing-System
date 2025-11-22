f647b484d2ce2b1249d86da896a2805d9d70f4ad
COthere03 <asicaasi28@gmail.com>
2025-11-22 22:49:33 +0800

    Add files via upload

---
+++ Ride-Sharing System/src/main/java/com/mycompany/quiz4/Car.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quiz4;
+
+/**
+ *
+ * @author 10280
+ */
+public class Car extends Vehicle implements Rideable {
+    private double lastDistance;
+    private double lastFare;
+
+    public Car(String licensePlate, String driverName, float baseFare) {
+        super(licensePlate, driverName, baseFare);
+    }
+
+    public double calculateFare(double distance) {
+        lastDistance = distance;
+        lastFare = baseFare + distance * 3.0;
+        return lastFare;
+    }
+
+    public String getVehicleType() {
+        return "Car";
+    }
+
+    public void startRide() {}
+
+    public void endRide() {}
+
+    public String getFareBreakdown() {
+        return String.format(
+                "Type of Vehicle: %s\n" +
+                "Driver Name: %s\n" +
+                "License Plate: %s\n" +
+                "Distance: %.2f km\n" +
+                "Fare: %.2f",
+                getVehicleType(), driverName, licensePlate, lastDistance, lastFare
+        );
+    }
+
+}
